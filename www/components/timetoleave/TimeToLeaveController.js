d343dc572bfea0a19417d8ec562cdab62f9c7696
Jonathan Herdt <jonathan.herdt@student.hpi.uni-potsdam.de>
2016-04-28 17:31:52 +0200

    Implemented a bare-bones version of the time to leave view.

--- www/components/timetoleave/TimeToLeaveController.js
+++ www/components/timetoleave/TimeToLeaveController.js
@@ -2,10 +2,54 @@ angular.module('app.controllers')
 
 .controller('TimeToLeaveController', function($scope, socket) {
 
-        // Jonas socket comm code START
-        socket.on('calendar', function (data) {
-          console.log(data);
-          // socket.emit('calendar_ack', { my: 'data' });
-        });
-        // Jonas socket comm code END
+    // Jonas socket comm code START
+    socket.on('calendar', function (data) {
+        console.log(data);
+        // socket.emit('calendar_ack', { my: 'data' });
+    });
+    // Jonas socket comm code END
+
+    $scope.parents = [
+        {
+            transit: [
+                {
+                    type: 'car',
+                    minutesLeft: 30
+                },
+                {
+                    type: 'walk',
+                    minutesLeft: 10
+                }
+            ],
+            picture: 'img/father.jpg'
+        },
+        {
+            transit: [
+                {
+                    type: 'bus',
+                    minutesLeft: 20
+                },
+                {
+                    type: 'walk',
+                    minutesLeft: 10
+                }
+            ],
+            picture: 'img/mother.png'
+        }
+    ];
+    $scope.children = [
+        {
+            transit: [
+                {
+                    type: 'walk',
+                    minutesLeft: 40
+                },
+                {
+                    type: 'bicycle',
+                    minutesLeft: 50
+                }
+            ],
+            picture: 'img/child1.jpg'
+        }
+    ];
 });
--- www/js/routes.js
+++ www/js/routes.js
@@ -8,8 +8,6 @@ angular.module('app.routes', [])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
-
-
   .state('timeToLeaveOverview', {
     url: '/timetoleave',
     templateUrl: 'components/timetoleave/timetoleave.html',
@@ -22,6 +20,6 @@ angular.module('app.routes', [])
     controller: 'TimelineController'
   });
 
-  $urlRouterProvider.otherwise('/timetoleave')
+  $urlRouterProvider.otherwise('/timetoleave');
 
 });
