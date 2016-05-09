74b71270c7fda2a77bea2bf61bbb624cf3e50c90
Latt.Johanna <latt.johanna@laboratories.telekom.de>
2016-05-09 12:54:00 +0200

    time to leave is displayed based on the calendar data for a day (if there are no more events today, nothing will be displayed since atm we only have the calendar data of one day, not of the next day)

--- www/components/timetoleave/TimeToLeaveController.js
+++ www/components/timetoleave/TimeToLeaveController.js
@@ -3,13 +3,72 @@ angular.module('app.controllers')
 .controller('TimeToLeaveController', function ($scope, storage) {
 	// get the users' calendars from the storage and listen to updates
 	$scope.calendars = storage.getCalendars();
+    retrieveLeaveData();
 
 	storage.subscribe($scope, function onStorageUpdated() {
 		$scope.calendars = storage.getCalendars();
+        retrieveLeaveData();
 		$scope.$apply();
+        console.log($scope.calendars);
 	});
+    
+    function retrieveLeaveData() {
+        $scope.parents = [];
+        
+        for (var i = 0; i < $scope.calendars.length; i++) {
+            var calendar = $scope.calendars[i];
+            
+            // set the user data
+            var parent = {
+                picture : calendar.picture, 
+                nextEvent : "",
+                transit : []
+            };
+            
+            // find the next event for the user
+            var nextEvent = {
+                msecsUntilStart : -1,
+                event : null
+            };
+            var now = new Date();
+            calendar.events.forEach(function(event) {
+                var msecsUntilEvent = (event.start - now);
+                if (msecsUntilEvent < 0) return; // we dont want events that are already over
+                if (nextEvent.msecsUntilStart == -1 || nextEvent.msecsUntilStart > msecsUntilEvent) {
+                    nextEvent.msecsUntilStart = msecsUntilEvent;
+                    nextEvent.event = event;
+                }
+            });
+            
+            // retrieve transit information for the next event
+            if (nextEvent.event != null) {
+                parent.nextEvent = nextEvent.event.title;
+                if (nextEvent.event.optimized_transit != undefined) {
+                    // new Date(oldDateObj.getTime() + diff*60000);
+                    var timeToLeaveBest = new Date(nextEvent.event.start.getTime() + nextEvent.event.optimized_transit.best.duration *60000);
 
-	$scope.parents = [
+                    var timeToLeaveSecondBest = new Date(nextEvent.event.start.getTime() + nextEvent.event.optimized_transit.alternative.duration *60000);
+                    parent.transit = [
+                      { 
+                          type : nextEvent.event.optimized_transit.best.name,
+                          minutesLeft : Math.round((timeToLeaveBest - now)/1000/60)
+                      },
+                        { 
+                          type : nextEvent.event.optimized_transit.alternative.name,
+                          minutesLeft : Math.round((timeToLeaveSecondBest - now)/1000/60)
+                      }
+
+                    ];
+                } 
+            } else {
+                parent.nextEvent = '- No more events today -';
+            }
+            
+            $scope.parents.push(parent);
+        };
+    };
+
+	/*$scope.parents = [
 		{
 			transit: [
 				{
@@ -36,7 +95,7 @@ angular.module('app.controllers')
             ],
 			picture: 'img/mother.png'
         }
-    ];
+    ];*/
 	$scope.children = [
 		{
 			transit: [
@@ -49,6 +108,7 @@ angular.module('app.controllers')
 					minutesLeft: 50
                 }
             ],
+            nextEvent: 'Template Event',
 			picture: 'img/child1.jpg'
         }
     ];
