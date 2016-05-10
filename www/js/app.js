4e0dcc1782315e44b95f9c92afaf8eb2cb7e50ad
Jonas <jonas.kemper.biz@gmail.com>
2016-05-10 12:12:04 +0200

    printing simulator data

--- www/js/app.js
+++ www/js/app.js
@@ -5,116 +5,131 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'app.controllers', 'app.routes'])
 
-.run(function ($ionicPlatform, $rootScope, $state, $ionicScrollDelegate, socket) {
-	$ionicPlatform.ready(function () {
-		if (window.cordova && window.cordova.plugins.Keyboard) {
-			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-			// for form inputs)
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	.run(function ($ionicPlatform, $rootScope, $state, $ionicScrollDelegate, socket) {
+		$ionicPlatform.ready(function () {
+			if (window.cordova && window.cordova.plugins.Keyboard) {
+				// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+				// for form inputs)
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-			// Don't remove this line unless you know what you are doing. It stops the viewport
-			// from snapping when text inputs are focused. Ionic handles this internally for
-			// a much nicer keyboard experience.
-			cordova.plugins.Keyboard.disableScroll(true);
-		}
-		if (window.StatusBar) {
-			StatusBar.styleDefault();
-		}
-	});
-
-	$rootScope.keyPress = function (event) {
-		if (event.keyCode === 65) {
-			if ($state.current.name === 'timeline' && $ionicScrollDelegate.getScrollPosition().left === 0) {
-				$state.go('timeToLeaveOverview');
-			} else if($state.current.name === 'timeToLeaveOverview'){
-				$state.go('carStatus');
+				// Don't remove this line unless you know what you are doing. It stops the viewport
+				// from snapping when text inputs are focused. Ionic handles this internally for
+				// a much nicer keyboard experience.
+				cordova.plugins.Keyboard.disableScroll(true);
 			}
-			$ionicScrollDelegate.scrollBy(-50, 0, true);
-		} else if (event.keyCode === 68) {
-			if ($state.current.name === 'timeToLeaveOverview') {
-				$state.go('timeline');
-			} else if($state.current.name === 'carStatus'){
-				$state.go('timeToLeaveOverview');
+			if (window.StatusBar) {
+				StatusBar.styleDefault();
 			}
-			$ionicScrollDelegate.scrollBy(50, 0, true);
-		} else if (event.keyCode === 32) {
-		}
-	};
-
-	socket.emit('clock - request all calendars', {
-		day: new Date()
-	});
-})
-
-
-.factory('storage', function ($rootScope) {
-
-	var calendars = [];
-
-	return {
-		// controllers can listen to changes of the storage
-		subscribe: function (scope, callback) {
-			var handler = $rootScope.$on('storage-has-changed', callback);
-			scope.$on('$destroy', handler);
-		},
-		updateCalendar: function (calendar) {
-			// find the calendar to replace (matching is done by user's name)
-			calendars.forEach(function (currentCal, index) {
-				if (currentCal.name == calendar.name) {
-					calendars[index] = calendar;
-					return;
-				}
-			});
-
-			// if this calendar doesnt exist yet, just add it
-			calendars.push(calendar);
-
-			// notify controllers using this factory, that the storage has been updated
-			$rootScope.$emit('storage-has-changed');
-		},
-		setCalendars: function (cals) {
-			calendars = cals;
-		},
-		getCalendars: function () {
-			return calendars;
-		}
-	};
-})
-
-.factory('socket', function ($rootScope, storage) {
-
-	var socket = io.connect("http://localhost:8080");
-
-	socket.on('clock - calendar update', function (calendar) {
-		// iterate over all events in the calendar to convert them into javascript Dates
-		calendar.events.forEach(function (event) {
-			event.start = new Date(event.start);
-			event.end = new Date(event.end);
 		});
-		// once that is done, save the updated calendar to the storage
-		storage.updateCalendar(calendar);
-	});
 
-	return {
-		on: function (eventName, callback) {
-			socket.on(eventName, function () {
-				var args = arguments;
-				$rootScope.$apply(function () {
-					callback.apply(socket, args);
-				});
-			});
-		},
-		emit: function (eventName, data, callback) {
-			socket.emit(eventName, data, function () {
-				var args = arguments;
-				$rootScope.$apply(function () {
-					if (callback) {
-						callback.apply(socket, args);
+		$rootScope.keyPress = function (event) {
+			if (event.keyCode === 65) {
+				if ($state.current.name === 'timeline' && $ionicScrollDelegate.getScrollPosition().left === 0) {
+					$state.go('timeToLeaveOverview');
+				} else if($state.current.name === 'timeToLeaveOverview'){
+					$state.go('carStatus');
+				}
+				$ionicScrollDelegate.scrollBy(-50, 0, true);
+			} else if (event.keyCode === 68) {
+				if ($state.current.name === 'timeToLeaveOverview') {
+					$state.go('timeline');
+				} else if($state.current.name === 'carStatus'){
+					$state.go('timeToLeaveOverview');
+				}
+				$ionicScrollDelegate.scrollBy(50, 0, true);
+			} else if (event.keyCode === 32) {
+			}
+		};
+
+		socket.emit('clock - request all calendars', {
+			day: new Date()
+		});
+	})
+
+
+	.factory('storage', function ($rootScope) {
+
+		var calendars = [];
+		var carSimulatorData = {};
+
+		return {
+			// controllers can listen to changes of the storage
+			subscribe: function (scope, callback) {
+				var handler = $rootScope.$on('storage-has-changed', callback);
+				scope.$on('$destroy', handler);
+			},
+			updateCalendar: function (calendar) {
+				// find the calendar to replace (matching is done by user's name)
+				calendars.forEach(function (currentCal, index) {
+					if (currentCal.name == calendar.name) {
+						calendars[index] = calendar;
+						return;
 					}
 				});
-			})
-		}
-	};
-});
+
+				// if this calendar doesnt exist yet, just add it
+				calendars.push(calendar);
+
+				// notify controllers using this factory, that the storage has been updated
+				$rootScope.$emit('storage-has-changed');
+			},
+			setCalendars: function (cals) {
+				calendars = cals;
+			},
+			getCalendars: function () {
+				return calendars;
+			},
+			updateCarSimulatorData: function (key, newCarSimulatorData) {
+				carSimulatorData[key] = newCarSimulatorData;
+
+				// notify controllers using this factory, that the storage has been updated
+				$rootScope.$emit('storage-has-changed');
+			},
+			getCarSimulatorData: function () {
+				return carSimulatorData;
+			}
+		};
+	})
+
+	.factory('socket', function ($rootScope, storage) {
+
+		var socket = io.connect("http://localhost:8080");
+
+		socket.on('clock - calendar update', function (calendar) {
+			// iterate over all events in the calendar to convert them into javascript Dates
+			calendar.events.forEach(function (event) {
+				event.start = new Date(event.start);
+				event.end = new Date(event.end);
+			});
+			// once that is done, save the updated calendar to the storage
+			storage.updateCalendar(calendar);
+		});
+
+		socket.on('[Car Simulator Data] - Battery Update', function (data) {
+			storage.updateCarSimulatorData('battery_level', data);
+			console.log('foo')
+		});
+
+		return {
+			on: function (eventName, callback) {
+				socket.on(eventName, function () {
+					var args = arguments;
+					$rootScope.$apply(function () {
+						callback.apply(socket, args);
+					});
+				});
+			},
+			emit: function (eventName, data, callback) {
+				socket.emit(eventName, data, function () {
+					var args = arguments;
+					$rootScope.$apply(function () {
+						if (callback) {
+							callback.apply(socket, args);
+						}
+					});
+				})
+			}
+		};
+	});
 
 angular.module('app.controllers', []);
