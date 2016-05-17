befe89ec5b48829ade3ea5d329a5f88b8e3fa04a
mfritzsche <martin.fritzsche@citrix.com>
2016-05-17 23:48:28 +0200

    make led's stop when exiting car status display

--- www/components/carstatus/CarStatusController.js
+++ www/components/carstatus/CarStatusController.js
@@ -29,4 +29,8 @@ angular.module('app.controllers')
                 });
             }
         });
+
+        $scope.$on("$ionicView.leave", function(event, data){
+            leds.toggleFlash();
+        });
     });
